f8d567254373d0981296ffc36d52b7b6fec95706
Tate <tatehakert@gmail.com>
2022-02-17 09:27:09 -0600

    updated html upload form

---
+++ classes/word-index.js
@@ -0,0 +1,12 @@
+
+class WordIndex{
+    constructor(){
+        this.dict = {}
+        this.nVal = 1
+        this.uniqueWordCount = 0
+        this.mostFrequent = []
+    }
+
+}
+
+module.exports = WordIndex;
